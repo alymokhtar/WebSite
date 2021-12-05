0a7dc3d7e4918a1feef64d2e0a8d3a838d3f106e
alymokhtar <alimokhtar23@gmail.com>
2021-12-05 06:05:42 +0200

    Add Counter To Event

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ span.onclick = function () {
     top: 0,
   });
 };
+
+// Start Event
+let counter = setInterval(() => {
+  let countDown = new Date("Mar 10, 2022 23:59:59").getTime();
+  let dateToday = new Date().getTime();
+  let diffDate = countDown - dateToday;
+
+  let days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  let minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+
+  let seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+  if (diffDate < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+// End Event
